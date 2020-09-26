4becbf460f2e861abde13fa758f06e0bf74d0a42
Will Harris <will.t.harris@protonmail.com>
2020-09-26 16:19:01 -0700

    chore: add card colors

--- src/shared/colors.js
+++ src/shared/colors.js
@@ -1,3 +1,5 @@
-export const placeholderBg = '#777777';
-export const placeholderTxt = '#DDDDDD';
-export const border = '#D8D8D8';
+export const placeholderBg = "#777777";
+export const placeholderTxt = "#DDDDDD";
+export const border = "#D8D8D8";
+export const timestampBg = "#F7F7F7";
+export const timestampTxt = '#6D757D';
